505b4bdbe27ff5f0b595be8bf466aec7b3e63e86
Brandon <brandon.tromp@coolgames.com>
2024-04-24 15:20:27 +0200

    fixed some styling

---
+++ app/navbar.tsx
@@ -0,0 +1,9 @@
+export default function NavBar() {
+    return (
+        <div className="navbar border-2">
+        <a className="btn btn-ghost text-xl">daisyUI</a>
+      </div>
+    )
+}
+
+
--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,5 @@
 export default function Home() {
   return (
-  <div className="p-10">
-    <button className="btn btn-primary">Button</button>
-  </div>
+<div></div>
   );
 }
